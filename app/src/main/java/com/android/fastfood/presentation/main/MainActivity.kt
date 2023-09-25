19787ab5e18c1f7a56a185d12bd6b82e06cdf48d
ivan080402 <ivansimbolon99@gmail.com>
2023-09-26 02:45:28 +0700

    final push

---
+++ app/src/main/java/com/android/fastfood/presentation/main/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.android.fastfood.presentation.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.fastfood.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
